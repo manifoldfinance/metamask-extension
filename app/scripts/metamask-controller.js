9ea444b3f94413c207c20ab0dea50f3171c8bf72
Alex Miller <asmiller1989@gmail.com>
2021-04-05 10:03:15 -0500

    Adds support for GridPlus Lattice1 hardware wallet

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -14,6 +14,7 @@ import { stripHexPrefix } from 'ethereumjs-util';
 import log from 'loglevel';
 import TrezorKeyring from 'eth-trezor-keyring';
 import LedgerBridgeKeyring from '@metamask/eth-ledger-bridge-keyring';
+import LatticeKeyring from 'eth-lattice-keyring'
 import EthQuery from 'eth-query';
 import nanoid from 'nanoid';
 import contractMap from '@metamask/contract-metadata';
@@ -332,7 +333,7 @@ export default class MetamaskController extends EventEmitter {
       preferencesController: this.preferencesController,
     });
 
-    const additionalKeyrings = [TrezorKeyring, LedgerBridgeKeyring];
+    const additionalKeyrings = [TrezorKeyring, LedgerBridgeKeyring, LatticeKeyring];
     this.keyringController = new KeyringController({
       keyringTypes: additionalKeyrings,
       initState: initState.KeyringController,
@@ -1341,7 +1342,8 @@ export default class MetamaskController extends EventEmitter {
         .map((address) => toChecksumHexAddress(address)),
       ledger: [],
       trezor: [],
-    };
+      lattice: [],
+    }
 
     // transactions
 
@@ -1431,7 +1433,7 @@ export default class MetamaskController extends EventEmitter {
   // Hardware
   //
 
-  async getKeyringForDevice(deviceName, hdPath = null) {
+  async getKeyringForDevice (deviceName, hdPath = null) {
     let keyringName = null;
     switch (deviceName) {
       case 'trezor':
@@ -1439,6 +1441,9 @@ export default class MetamaskController extends EventEmitter {
         break;
       case 'ledger':
         keyringName = LedgerBridgeKeyring.type;
+        break;
+      case 'lattice':
+        keyringName = LatticeKeyring.type;
         break;
       default:
         throw new Error(
@@ -1449,12 +1454,13 @@ export default class MetamaskController extends EventEmitter {
       keyringName,
     )[0];
     if (!keyring) {
-      keyring = await this.keyringController.addNewKeyring(keyringName);
+      keyring = await this.keyringController.addNewKeyring(keyringName)
     }
     if (hdPath && keyring.setHdPath) {
       keyring.setHdPath(hdPath);
     }
-
+    if (deviceName === 'lattice')
+      keyring.appName = 'MetaMask';
     keyring.network = this.networkController.getProviderConfig().type;
 
     return keyring;
